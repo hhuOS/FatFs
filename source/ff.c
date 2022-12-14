56c544d93c4155856e8934884a7e55b00e3d631a
Fabian Ruhland <ruhland@hhu.de>
2022-12-14 15:47:20 +0100

    Apply patch from 2022-11-11

--- source/ff.c
+++ source/ff.c
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------/
-/  FatFs - Generic FAT Filesystem Module  R0.15                               /
+/  FatFs - Generic FAT Filesystem Module  R0.15 w/patch1                      /
 /-----------------------------------------------------------------------------/
 /
 / Copyright (C) 2022, ChaN, all right reserved.
@@ -900,7 +900,7 @@ static int lock_volume (	/* 1:Ok, 0:timeout */
 	int rv;
 
 
-#if FS_LOCK
+#if FF_FS_LOCK
 	rv = ff_mutex_take(fs->ldrv);	/* Lock the volume */
 	if (rv && syslock) {			/* System lock reqiered? */
 		rv = ff_mutex_take(FF_VOLUMES);	/* Lock the system */
